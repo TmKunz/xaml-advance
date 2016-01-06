060f2c23f7729678fb2710034510cc61d771691c
TmKunz <tami.jh@gmail.com>
2016-01-06 13:43:21 +0100

    DEV206.2x HW1
    Homework
    Restaurant Manager

---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            this.OrderItems = base.Repository.Orders;
+        }
+
+        private List<Order> orderitems;
+        public List<Order> OrderItems
+        {
+            get { return orderitems; }
+            set { orderitems = value; this.OnPropertyChanged(); }
+        }
+    }
+}
---
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantManager.Models
+{
+    public class OrderDataManager : DataManager
+    {       
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+            this.CurrentlySelectedMenuItems = new List<MenuItem>
+            {
+                this.MenuItems[3],
+                this.MenuItems[5]
+            };
+        }
+
+        private List<MenuItem> menuitems;
+        public List<MenuItem> MenuItems
+        {
+            get { return menuitems; }
+            set { menuitems = value; this.OnPropertyChanged(); }
+        }
+
+        private List<MenuItem> currentlyselectedmenuitems;
+        public List<MenuItem> CurrentlySelectedMenuItems
+        {
+            get { return currentlyselectedmenuitems; }
+            set {
+                currentlyselectedmenuitems = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+}
